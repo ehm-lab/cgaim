ee6961ea51a00b81034f0a3928ce8487fbe30bbe
PierreMasselot <pierre.masselot@ete.inrs.ca>
2018-09-14 08:49:29 -0400

    GAIM : Initialization of repository
    Files added:
    	- Main functions.R: the main functions of the GAIM developed
    	- Secondary functions.R: functions useful for the model but not directly called
    	- Alternative functions.R: tests for alternative GAIMs

---
+++ Alternative functions.R
@@ -0,0 +1,202 @@
+#! The method of Yu and Ruppert (2002)
+#! Difference: use of a LM instead of NLS for initial beta values
+si.yr <- function(y, x, smoother = "ns", smoother.pars = list(df = 10), lambda = 1) 
+{
+    x <- as.matrix(x)
+    p <- ncol(x)
+    n <- nrow(x)
+    # initialize alphas
+    alpha <- alpha.init(type = "ols", p = p, y = y, x = x, normalize = TRUE, first.pos = TRUE)
+    names(alpha) <- sprintf("alpha%i", 1:p)
+    calpha <- alpha[-1] / alpha[1]
+    z <- x %*% alpha
+    smoother.pars$x <- z
+    bases <- do.call(smoother, smoother.pars)
+    nb <- ncol(bases)
+    # augment data for penalized splines
+    d <- diag(c(rep(0,1+p), rep(1,nb-p-1)))
+    x.aug <- rbind(bases, sqrt(lambda)*d^(1/2))
+    y.aug <- c(y, rep(0, nb))
+    aug.data <- data.frame(y = y.aug, x = x.aug)
+    # fit penalized splines for initial basis coefficients
+    bcoef.init <- coef(lm(y ~ 0 + ., data = aug.data))
+    # Define objective function
+    obj.fun <- function(y, x, lambda, d, calpha, beta, smoother.pars){
+        z <- x %*% param.map(calpha)
+        smoother.pars$x <- z
+        basis <- do.call(smoother, smoother.pars)
+        nb <- ncol(basis)        
+        pen <- sqrt(lambda)*d
+        aug.dat <- rbind(basis, pen)
+        aug.y <- c(y, rep(0, nb))
+        aug.y - aug.dat%*%beta
+    }
+    # Fit NLS
+    nls.fit <- nls(~ obj.fun(y, x, lambda, d, alpha, beta, smoother.pars), data = list(y = y, x = x, lambda = lambda, d = d, smoother.pars = smoother.pars), start = list(alpha = calpha, beta = bcoef.init), control = nls.control(warnOnly = T))  
+    final.alpha <- param.map(coef(nls.fit)[1:(p-1)])
+    final.z <- x %*% final.alpha
+    final.beta <- coef(nls.fit)[p:length(coef(nls.fit))]
+    smoother.pars$x <- final.z
+    Bmat <- do.call(smoother, smoother.pars)
+    final.gz <- Bmat %*% final.beta
+    # GCV criterion (ou K-fold?)
+#    cminv <- solve(t(Bmat)%*%Bmat + n*lambda*d)
+#    As <- Bmat %*% cminv %*% t(Bmat)
+#    trHat <- sum(diag(cminv%*%t(Bmat)%*%Bmat)) + sum(diag(solve(t(Bmat)%*%(diag(1,n)-As)%*%Bmat) %*% t(Bmat)%*%(diag(1,n) - As)^2%*%Bmat))
+    output <- list(alpha = final.alpha, z = final.z, gz = final.gz, basis_coefs = final.beta)
+    return(output)
+}
+
+param.map <- function(gamma){
+    c(1, gamma) / sqrt(1 + sum(gamma^2))
+}
+
+p.spline.expansion <- function(expr, degree = 3, knots = NULL, nknots = 9, intercept = TRUE, coef.name = "beta")
+# The one of Yu and Ruppert (2002)
+# Same arguments as ns
+{
+    if (is.null(knots)){
+       knots <- seq(0,1, length.out = nknots + 2)[-c(1,nknots+2)]
+    }
+    polys <- sprintf("(%s)^%i", expr, 1:degree)
+    splines <- sprintf("((%1$s) - quantile(%1$s, %2$f))^%3$i", expr, knots, degree)
+    basis <- c(polys, splines)
+    if (intercept) basis <- c("1", basis)
+    nb <- length(basis)
+    coefs <- sprintf("%s%i", coef.name, 1:(nb+intercept)-intercept)
+    cbprod <- paste(coefs, basis, sep = "*")
+    final.form <- paste(cbprod, collapse = "+")
+    return(list(formula = as.formula(final.form), coef.names = coefs))
+}
+
+# An additive version of Yu and Ruppert (2002) SI model
+aim.yr <- function(x, y, w, smoother = "ns", smoother.args = list(NULL), smoother.penalty = list(NULL), lambdas = 1, control, init.type = "runif", trace = T)
+# x: a list of matrices giving the variables for each index. The matrices need have the same number of rows (individuals) but can have diffeent numbers of columns (variables). If a matrix is provided, a single-index model is fitted. 
+# y: a numeric vector containing the response observations
+# w: weigths (TODO!)
+# smoother: a character vector giving the smoother used for each index. Can be different for each index. Recycled if necessary.
+# smoother.args: a (nested) list containing optional arguments for each smoother.
+# smoother.penalty: a list containing penalty matrices for each index. 
+# lambdas: a vector of penaly coefficients for each index
+# control: control parameters for nls algorithm. See ?nls.control
+# init.type: intialization of alphas TODO
+# trace: if true, keep criteria and parameters values of each iteration TODO
+{
+    if (!is.list(x)) x <- list(x)
+    xnames <- names(x)
+    n <- length(y)
+    p <- length(x)
+    pvec <- sapply(x, ncol)
+    if (any(sapply(x, nrow) != n)) stop("Matrices in x must have number of rows equal to the length of y")
+    smoother <- rep_len(smoother, p)
+    if (length(smoother.args) != p){
+       warning("Smoother.args does not have the same length as x and is thus recycled.")
+       smoother.args <- rep_len(smoother.args, p)
+    }
+    if (length(smoother.penalty) != p){
+       warning("Smoother.penalty does not have the same length as x and is thus recycled.")
+       smoother.penalty <- rep_len(smoother.penalty, p)
+    }
+    lambdas <- rep_len(lambdas, p)
+    if(missing(w)) w <- rep(1/n,n)    
+    # initialize alphas
+    X <- Reduce(cbind, x)
+    alpha <- alpha.init(type = "ols", p = p, y = y, x = X, normalize = TRUE, first.pos = F)
+    names(alpha) <- NULL
+    alphas <- split(alpha, rep(1:p, pvec))
+    names(alphas) <- xnames
+    calpha <- sapply(alphas, function(x) x[-1] / x[1])
+    # Compute first z
+    zs <- mapply("%*%", x, alphas, SIMPLIFY = F)
+    # initialize betas
+    bases <- vector("list", p)
+    nb <- vector("integer", p)
+    for (j in 1:p){
+        smoother.args[[j]]$x <- zs[[j]]
+        bases[[j]] <- do.call(smoother[j], smoother.args[[j]])
+        nb[j] <- ncol(bases[[j]])
+    }
+    allbases <- Reduce(cbind, bases)
+    for (j in 1:p) if(is.null(smoother.penalty[[j]])) smoother.penalty[[j]] <- diag(c(rep(0,1+pvec[j]), rep(1,nb[j]-pvec[j]-1)))
+    Smat <- mapply("*", smoother.penalty, lambdas, SIMPLIFY = F)
+    allS <- as.matrix(bdiag(Smat))
+    aug.data <- data.frame(y = c(y, rep(0, sum(nb))), x = rbind(allbases, mat.sqrt(allS)))
+    bcoef.init <- coef(lm(y ~ 0 + ., data = aug.data))
+    # Define objective function
+    obj.fun <- function(y, x, allS, calpha, beta, smoother.args){
+        calpha <- split(calpha, rep(1:p, pvec-1))
+        alphas <- lapply(calpha, param.map)
+        zs <- mapply("%*%", x, alphas, SIMPLIFY = F)
+        bases <- vector("list", p)
+        for (j in 1:p){
+            smoother.args[[j]]$x <- zs[[j]]
+            bases[[j]] <- do.call(smoother[j], smoother.args[[j]])
+        }
+        allbases <- Reduce(cbind, bases)     
+        aug.dat <- rbind(allbases, allS)
+        aug.y <- c(y, rep(0, sum(nb)))
+        aug.y - aug.dat%*%beta
+    }
+    # Fit NLS
+    nls.fit <- nls(~ obj.fun(y, x, allS, calpha, beta, smoother.args), data = list(y = y, x = x, allS = allS, smoother.args = smoother.args), start = list(calpha = unlist(calpha), beta = bcoef.init), control = nls.control(warnOnly = T))
+    fit.coef <- coef(nls.fit)
+    final.calpha <- fit.coef[grep('calpha', names(fit.coef))]
+    names(final.calpha) <- NULL
+    final.calpha <- split(final.calpha, rep(1:p, pvec-1))
+    final.alpha <- lapply(final.calpha, param.map)
+    names(final.alpha) <- xnames
+    final.z <- mapply("%*%", x, final.alpha)
+    final.bases <- vector("list", p)
+    for (j in 1:p){
+        smoother.args[[j]]$x <- final.z[,j]
+        final.bases[[j]] <- do.call(smoother[j], smoother.args[[j]])
+    }
+    final.beta <- split(fit.coef[grep('beta', names(fit.coef))], rep(1:p,nb))
+    final.gz <- mapply("%*%", final.bases, final.beta)
+    final.gz <- scale(final.gz, scale = F)
+    beta0 <- sum(attr(final.gz, "scaled:center"))
+    betas <- apply(final.gz, 2, function(x) sqrt(sum(x^2)))
+    for (j in 1:p) final.gz[,j] <- final.gz[,j] / betas[j]
+    output <- list(alpha = final.alpha, gz = final.gz, z = final.z, beta = c(beta0,betas))
+    return(output)
+}
+
+#! Matrix square root
+mat.sqrt<-function(S) # A simple matrix square root
+{ 
+    d<-eigen(S,symmetric=TRUE)
+    rS<-d$vectors%*%diag(d$values^0.5)%*%t(d$vectors)
+    return(rS)
+}
+
+#! A naïve AIM that attempts to integrate the remarks of Wang et al. (2015) that the index coefficients can be retrieved by LS
+aim.naive <- function(x, y, w, gam.pars = list(), control, trace = T)
+{
+    if (!is.list(x)) x <- list(x)
+    xnames <- names(x)
+    n <- length(y)
+    p <- length(x)
+    pvec <- sapply(x, ncol)
+    if (any(sapply(x, nrow) != n)) stop("Matrices in x must have number of rows equal to the length of y")
+    if(missing(w)) w <- rep(1/n,n)    
+    # estimate alphas
+    X <- Reduce(cbind, x)
+    alpha <- alpha.init(type = "ols", p = p, y = y, x = X, normalize = F, first.pos = F)
+    names(alpha) <- NULL
+    alphas <- split(alpha, rep(1:p, pvec))
+    names(alphas) <- xnames
+    nor.alphas <- lapply(alphas, function(x) x/sqrt(sum(x^2)))
+    # compute zs
+    zs <- mapply("%*%", x, nor.alphas)
+    # estimate gs
+    gam.pars$data <- data.frame(y = y, zs)
+    gam.pars$formula <- as.formula(sprintf("y ~ %s", paste(sprintf("s(%s)", names(gam.pars$data)[-1]), collapse = "+")))
+    gz.fit <- do.call(gam, gam.pars)
+    gz <- predict(gz.fit, type = "terms")
+    final.gz <- scale(gz, scale = F)
+    beta0 <- sum(attr(final.gz, "scaled:center"))
+    betas <- apply(final.gz, 2, function(x) sqrt(sum(x^2))) 
+    for (j in 1:p) final.gz[,j] <- final.gz[,j] / betas[j]
+    output <- list(alpha = alphas, gz = final.gz, z = zs, beta = c(beta0,betas))
+    return(output)   
+}
---
+++ Main functions.R
@@ -0,0 +1,157 @@
+#!#############################################################################
+#!
+#!                        Main GAIM functions                                  
+#!
+#!#############################################################################
+
+source("C:/Users/masselpl/Documents/Recherche/2017-2019 - Post-doc/Programmes/R/2 - Indices PPR/Package functions/Secondary functions.R")
+
+#! fit single term ppr
+single.term <- function(x, y, w, alpha = alpha.init(ncol(x)), beta1 = 1, smoother = "spline", smoother.args = NULL, control = NULL) 
+# y: vector of response values
+# x: matrix of predictor values
+# w: weights
+# beta1: initial value of beta1 (possibly inherited from previous passes by the backfitting)
+# smoother: the smoother used
+# control: control parameters for the search algorithm
+{
+    def.control <- list(tol = 5e-3, max.iter = 20, min.step.len = 0.1, halving = T)
+    control <- c(control,def.control[!names(def.control) %in% names(control)])
+    smooth.fun <- sprintf("%s.smoother", smoother)
+    x <- as.matrix(x)
+    p <- ncol(x)
+    n <- nrow(x)
+    if(missing(w)) w <- rep(1/n,n)
+    # First iteration
+#    alpha <- alpha.init(type = "ols", p = p, y = y, x = x, normalize = TRUE)
+    g <- do.call(smooth.fun, c(list(y = y/beta1, x = x, alpha = alpha, w = w), smoother.args))
+#    x11(); plot(x%*%alpha, g$gz)
+#    z <- x %*% alpha
+#    sm.fit <- do.call("smooth.spline", list(x = z, y = y/beta1))
+#    gz <- predict(sm.fit, z)$y
+#    gznorm <- sqrt(sum(g$gz^2))
+#    g$gz <- g$gz / gznorm
+#    beta1 <- beta1*gznorm
+    l2 <- L2(y, beta1*g$gz, w)
+    eps <- (var(y) - l2)/var(y)
+    # Iterate until convergence
+    c1 <- 1
+    while (eps > control$tol && c1 <= control$max.iter){        
+#        dgz <- do.call(deriv.fun, list(x = x, z = z, gz = gz, alpha = alpha)) #! TODO: Modify to allow passing function for derivation<
+#        dgz <- predict(sm.fit, z, deriv = 1)$y 
+        # As in Roosen and Hastie (1994)
+        r <- y - g$gz
+        xf <- beta1 * x * matrix(g$dgz, nrow = n, ncol = p, byrow = F)
+        delta <- coef(lm(r ~ 0 + xf, weights = w))
+#        delta <- apply(beta1*x, 2, function(x){
+#            xf <- x*g$dgz
+#            coef(lm(r ~ 0 + xf, weights = w))
+#        })
+        cuts <- 1
+        l2.old <- l2
+        repeat {   #Loop for halving steps in case of bad step  #
+            delta <- delta * cuts
+            alpha.new <- alpha + delta
+#            alpha.new <- sign(alpha.new[1])*alpha.new/sqrt(sum(alpha.new^2)) # For identifiability
+            g <- do.call(smooth.fun, c(list(y = y/beta1, x = x, alpha = alpha.new, w = w), smoother.args))
+#            z <- x %*% alpha.new
+#            sm.fit <- do.call(smoother, list(x = z, y = y/beta1))
+#            gz <- predict(sm.fit, z)$y
+#            gznorm <- sqrt(sum(g$gz^2))
+#            g$gz <- g$gz / gznorm
+#            beta1 <- beta1*gznorm
+            l2 <- L2(y, beta1*g$gz, w)
+            if (l2 < l2.old || cuts < control$min.step.len || !control$halving){
+               break
+            }
+            cuts <- cuts / 2
+        }
+        alpha <- alpha.new
+        eps <- (l2.old - l2)/l2.old
+        c1 <- c1 + 1
+#        plot(x%*%alpha, g$gz)
+#        print(alpha)
+    }
+    alpha <- alpha/sqrt(sum(alpha^2)) 
+    z <- x %*% alpha
+    g <- do.call(smooth.fun, c(list(y = y/beta1, x = x, alpha = alpha, w = w), smoother.args))
+#    sm.fit <- do.call(smoother, list(x = z, y = y/beta1))
+#    gz <- predict(sm.fit, z)$y
+    gznorm <- sqrt(sum(g$gz^2))
+    g$gz <- g$gz / gznorm
+    beta1 <- beta1*gznorm
+    return(list(alpha = alpha, z = z, gz = g$gz, beta = beta1)) #! TODO: add predict
+}
+
+#! Additive index models
+aim <- function(x, y, w, smoother = "spline", smoother.args = list(NULL), control = NULL, init.type = "runif", trace = T) 
+# x: a list of matrices giving the variables for each index. The matrices need have the same number of rows (individuals) but can have diffeent numbers of columns (variables). If a matrix is provided, a single-index model is fitted. 
+# y: a numeric vector containing the response observations
+# w: weigths
+# smoother: a character vector giving the smoother used for each index. Can be different for each index. Recycled if necessary.
+# smoother.args: a (nested) list containing optional arguments for each smoother. 
+# control: control parameters for the search algorithm
+# trace: if true, keep criteria and parameters values of each iteration
+{
+    #! Estimate all coefficients first? As in Wang et al. (2015)
+    if (!is.list(x)) x <- list(x)
+    xnames <- names(x)
+    n <- length(y)
+    p <- length(x)
+    pvec <- sapply(x, ncol)
+    if (any(sapply(x, nrow) != n)) stop("Matrices in x must have number of rows equal to the length of y")
+    smoother <- rep_len(smoother, p)
+    if (length(smoother.args) != p){
+       warning("Smoother.args does not have the same length as x and is thus recycled.")
+       smoother.args <- rep_len(smoother.args, p)
+    }
+    if(missing(w)) w <- rep(1/n,n)
+    def.control <- list(bf.tol = 5e-03, bf.maxit = 50)
+    control <- c(control,def.control[!names(def.control) %in% names(control)]) 
+    # Initialize variables
+    beta0 <- mean(y) #! Possibly remove for the fisher scoring
+    betas <- rep(1,p)
+    alphas <- mapply(alpha.init, p = pvec, x = x, MoreArgs = list(y = y, type = init.type))
+    gs <- matrix(0, n, p)
+    # Backfitting
+    rel.delta <- control$bf.tol + 1
+    cbf <- 0
+    if (trace){ # Tracing the algorithm evolution
+        trace.list <- list(
+            criteria = matrix(NA, nrow = control$bf.maxit, ncol = 3, dimnames = list(NULL, c("delta", "rel.delta", "rss"))), 
+            beta = matrix(NA, nrow = control$bf.maxit, ncol = p, dimnames = list(NULL, xnames)), 
+            alpha = mapply(matrix, ncol = pvec, MoreArgs = list(nrow = control$bf.maxit)), 
+            gz = replicate(p, matrix(nrow = n, ncol = control$bf.maxit), simplify = F)
+        )
+        names(trace.list$gz) <- xnames
+    }
+    while (rel.delta > control$bf.tol && cbf < control$bf.maxit){
+        r <- y - beta0 - (gs %*% betas)
+        deltaf <- 0
+        for (j in 1:p){ #! /!\ Oscillation of betas and gz
+            rj <- r + betas[j]*gs[,j]
+            jterm <- single.term(x = x[[j]], y = rj, w = w, alpha = alphas[[j]], beta1 = betas[j], smoother = smoother[j], smoother.args = smoother.args[[j]], control = control)
+            betas[j] <- jterm$beta 
+            alphas[[j]] <- jterm$alpha
+            deltaf <- deltaf + weighted.mean((jterm$gz - gs[,j])^2, w)  
+            gs[,j] <- jterm$gz - mean(jterm$gz)
+            beta0 <- beta0 + betas[j]*mean(jterm$gz)
+        }
+        rel.delta <- sqrt(deltaf/sum(apply(gs^2, 2, weighted.mean, w = w))) 
+        cbf <- cbf + 1
+        if (trace){ # Tracing the algorithm evolution
+           trace.list$criteria[cbf,] <- c(deltaf, rel.delta, sum(r^2))
+           trace.list$beta[cbf,] <- betas
+           for (i in 1:p){
+               trace.list$alpha[[i]][cbf,] <- alphas[[i]]
+               trace.list$gz[[i]][,cbf] <- gs[,i]
+           } 
+        }
+    }
+    if (cbf == control$bf.maxit) warning(sprintf("Convergence not attained after %i iterations", control$bf.maxit))
+    zs <- mapply("%*%", x, alphas)
+    output <- list(alpha = alphas, gz = gs, z = zs, beta = c(beta0, betas))
+    if (trace) output$trace <- trace.list
+    return(output)
+}
+
